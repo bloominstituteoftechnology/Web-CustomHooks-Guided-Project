4a0c790ad0d08895f2d7c3d4554e4b672b0bee72
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-10-02 08:37:03 +0200

    wip

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,47 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const todosApi = 'http://localhost:4000/todos';
 
-export default class Todos extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: [],
-    };
-  }
+export default function Todos({ user }) {
+  const [todos, setTodos] = useState([]);
 
-  componentDidMount() {
-    axios.get(todosApi).then(res => {
-      this.setState({ todos: res.data });
-    });
-  }
+  useEffect(() => {
+    axios.get(todosApi).then(res => setTodos(res.data));
+  }, []);
 
-  markTodo = (id, isComplete) => event => {
-    this.setState(currentState => {
-      return {
-        todos: currentState.todos.map(todo => {
-          if (todo.id !== id) return todo;
-          return { id: todo.id, name: todo.name, complete: isComplete };
-        }),
-      };
-    });
-  }
+  const markTodo = (id, isComplete) => event => {
+    setTodos(todos.map(todo => {
+      if (todo.id !== id) return todo;
+      return { id: todo.id, name: todo.name, complete: isComplete };
+    }));
+  };
 
-  render() {
-    return (
-      <div className='component' style={{ borderColor: 'gold' }}>
-        <h5>{this.props.user}&apos;s todos:</h5>
-        {
-          this.state.todos.map((todo) => (
-            <div key={todo.id}>
-              {todo.name} {!todo.complete && 'NOT '}completed
-              <button onClick={this.markTodo(todo.id, true)}>Mark done</button>
-              <button onClick={this.markTodo(todo.id, false)}>Mark undone</button>
-            </div>
-          ))
-        }
-      </div>
-    );
-  }
+  return (
+    <div className='component' style={{ borderColor: 'gold' }}>
+      <h5>{user}&apos;s todos:</h5>
+      {
+        todos.map((todo) => (
+          <div key={todo.id}>
+            {todo.name} {!todo.complete && 'NOT '}completed
+            <button onClick={markTodo(todo.id, true)}>Mark done</button>
+            <button onClick={markTodo(todo.id, false)}>Mark undone</button>
+          </div>
+        ))
+      }
+    </div>
+  );
 }

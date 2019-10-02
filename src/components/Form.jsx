7ed8d097f69684dfbdd3ee6836aa38ba377f2509
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-10-02 09:40:22 +0200

    wip

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
 
 export default function Form({ user }) {
-  const [formValues, setFormValues] = useState({
-    fname: '',
-    lname: '',
-  });
+  const [fname, setFname] = useState('');
+  const [lname, setLname] = useState('');
 
-  const onValueChange = event => setFormValues({
-    ...formValues,
-    [event.target.name]: event.target.value,
-  });
+  const onFnameChange = evt => {
+    setFname(evt.target.value);
+  };
+
+  const onLnameChange = evt => {
+    setLname(evt.target.value);
+  };
 
   const onFormSubmit = event => {
     event.preventDefault();
-    alert(`submitting ${formValues.lname}, ${formValues.fname}`);
+    alert(`submitting ${lname}, ${fname}`);
   };
 
   return (
@@ -24,11 +25,11 @@ export default function Form({ user }) {
     >
       <h5>{user}&apos;s best friend:</h5>
       <label>first name
-        <input value={formValues.fname} onChange={onValueChange} name='fname' />
+        <input value={fname} onChange={onFnameChange} />
       </label><br />
 
       <label>last name
-        <input value={formValues.lname} onChange={onValueChange} name='lname' />
+        <input value={lname} onChange={onLnameChange} />
       </label><br />
 
       <button>submit</button>

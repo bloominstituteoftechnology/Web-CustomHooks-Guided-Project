d0f1c059f0981751c5ee3bb8a8eb4dda8dea9d9e
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-12-04 10:40:15 +0100

    wip

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,24 @@
 import React, { useState } from 'react';
 
-export default function Counter({ user }) {
+// this would be done in an external file
+// for easy access (perhaps a hooks file??)
+export function useCounter() {
   const [count, setCount] = useState(0);
 
   const increment = event => setCount(count + 1);
   const decrement = event => setCount(count - 1);
 
+  return [count, increment, decrement];
+}
+// the end
+
+export default function Counter({ user }) {
+  const [theFunkyCount, increment, decrement] = useCounter();
+
   return (
     <div style={{ borderColor: 'red' }} className='component'>
       <h5>{user}&apos;s count:</h5>
-      <div>The count is {count}</div>
+      <div>The count is {theFunkyCount}</div>
       <button onClick={increment}>increment</button>
       <button onClick={decrement}>decrement</button>
     </div>
